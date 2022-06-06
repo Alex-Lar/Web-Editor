83b7d0ca58452e5fb1b9ca7c74674293c789972e
Alexander Larin <doremilarin@gmail.com>
2022-06-06 09:23:41 +0300

    added func for bold & italic text

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function main() {
 
     editor.addEventListener('keyup', addTextInStorage);
     boldBtn.addEventListener('click', makeTextBold);
+    italicBtn.addEventListener('click', makeTextItalic);
 
     function addTextInStorage() {
         localStorage.setItem('editor_text', editor.innerHTML);
@@ -19,6 +20,17 @@ function main() {
             let select = window.getSelection().toString();
             let userText = localStorage.getItem('editor_text');
             userText = userText.replace(select, `<b>${select}</b>`);
+
+            localStorage.setItem('editor_text', userText);
+            editor.innerHTML = localStorage.getItem('editor_text');
+        }
+    }
+
+    function makeTextItalic() {
+        if (window.getSelection()) {
+            let select = window.getSelection().toString();
+            let userText = localStorage.getItem('editor_text');
+            userText = userText.replace(select, `<em>${select}</em>`);
 
             localStorage.setItem('editor_text', userText);
             editor.innerHTML = localStorage.getItem('editor_text');
