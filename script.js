3dde15fee297d321cf0046586547f12cf111842d
Alexander Larin <doremilarin@gmail.com>
2022-06-06 09:03:14 +0300

    project init

---
+++ script.js
@@ -0,0 +1,30 @@
+function main() {
+    let editor = document.getElementById('editor-page');
+    const boldBtn = document.getElementById('bold-button');
+    const italicBtn = document.getElementById('italic-button');
+    
+    if (editor !== null) {
+        editor.innerHTML = localStorage.getItem('editor_text');
+    }
+
+    editor.addEventListener('keyup', addTextInStorage);
+    boldBtn.addEventListener('click', makeTextBold);
+
+    function addTextInStorage() {
+        localStorage.setItem('editor_text', editor.innerHTML);
+    }
+
+    function makeTextBold() {
+        if (window.getSelection()) {
+            let select = window.getSelection().toString();
+            let userText = localStorage.getItem('editor_text');
+            userText = userText.replace(select, `<b>${select}</b>`);
+
+            localStorage.setItem('editor_text', userText);
+            editor.innerHTML = localStorage.getItem('editor_text');
+        }
+    }
+}
+main();
+
+
